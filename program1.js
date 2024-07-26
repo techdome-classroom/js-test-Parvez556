c2e2a8bf65aeab5756cb3f7b6bf5e599ae5a8aa3
Parvez556 <husainparvez769@gmail.com>
2024-07-26 12:43:01 +0530

    All test cases are passed

--- program1.js
+++ program1.js
@@ -1,8 +1,39 @@
 const getTotalIsles = function (grid) {
+// write your code here
+
+//We check our grid id grid is 0 and length of grid ===0 we simply return 0.
+  if (grid == null || grid.length === 0) {
+    return 0;
+}
+
+let numberOfIslands = 0;
+
+function dfs(grid, i, j) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 'W') {
+        return;
+    }
+
+    grid[i][j] = 'W'; // Mark this cell as visited
+
+    // Check all four directions (up, down, left, right)
+    dfs(grid, i - 1, j); // up case
+    dfs(grid, i + 1, j); // down case
+    dfs(grid, i, j - 1); // left case
+    dfs(grid, i, j + 1); // right case
+}
+
+for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === 'L') {
+            numberOfIslands++;
+            dfs(grid, i, j);
+        }
+    }
+}
+
+return numberOfIslands;
+}
 
 
-  // write your code here
-
-};
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,31 @@
+
 const decodeTheRing = function (s, p) {
+  const m = s.length;
+  const n = p.length;
+ 
+  // initialize dp as false and dp [0][0] is true  
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(false));
+  dp[0][0] = true;
 
-    // write your code here
+  // If pattern start with * it is match as an empty message
+  for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === '*') {
+          dp[0][j] = dp[0][j - 1];
+      }
+  }
 
-  };
+  // With the help of nested loops we fills the DP table
+  for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+          if (p[j - 1] === '*') {
+              dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+          } else if (p[j - 1] === '?' || p[j - 1] === s[i - 1]) {
+              dp[i][j] = dp[i - 1][j - 1];
+          }
+      }
+  }
+
+  return dp[m][n];
+};
   
   module.exports = decodeTheRing;
